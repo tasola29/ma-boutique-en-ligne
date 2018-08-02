74130aaf7aa40c864f1bc9c815ae63f01106f70e
Solenn LARZUL <solennlarzul@gmail.com>
2018-08-02 15:11:01 +0200

    modif title
    Ma boutique en ligne

---
+++ index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="style.css">
+	
+    <title>Ma boutique</title>
+  </head>
+  <body>
+	
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Ma boutique en ligne</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Panier</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Boutique</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catégories</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown04">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Rechercher">
+        </form>
+      </div>
+    </nav>
+  
+	<section class="container ma-section">
+    
+		<h1 class="text-center mt-4 display-4">Welcome To All of You</h1>
+
+	</section>
+	
+		<form>
+		  <div class="form-row">
+			<div class="form-group col-md-6">
+			  <label for="inputEmail4">Email</label>
+			  <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+			</div>
+			<div class="form-group col-md-6">
+			  <label for="inputPassword4">Password</label>
+			  <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+			</div>
+		  </div>
+		  <div class="form-group">
+			<label for="inputAddress">Address</label>
+			<input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+		  </div>
+		  <div class="form-group">
+			<label for="inputAddress2">Address 2</label>
+			<input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+		  </div>
+		  <div class="form-row">
+			<div class="form-group col-md-6">
+			  <label for="inputCity">City</label>
+			  <input type="text" class="form-control" id="inputCity">
+			</div>
+			<div class="form-group col-md-4">
+			  <label for="inputState">State</label>
+			  <select id="inputState" class="form-control">
+				<option selected>Choose...</option>
+				<option>...</option>
+			  </select>
+			</div>
+			<div class="form-group col-md-2">
+			  <label for="inputZip">Zip</label>
+			  <input type="text" class="form-control" id="inputZip">
+			</div>
+		  </div>
+		  <div class="form-group">
+			<div class="form-check">
+			  <input class="form-check-input" type="checkbox" id="gridCheck">
+			  <label class="form-check-label" for="gridCheck">
+				Check me out
+			  </label>
+			</div>
+		  </div>
+		  <button type="submit" class="btn btn-primary">Sign in</button>
+		</form>
+	
+		<nav aria-label="Page navigation example">
+		  <ul class="pagination">
+			<li class="page-item">
+			  <a class="page-link" href="#" aria-label="Previous">
+				<span aria-hidden="true">&laquo;</span>
+				<span class="sr-only">Précédent</span>
+			  </a>
+			</li>
+			<li class="page-item"><a class="page-link" href="#">1</a></li>
+			<li class="page-item"><a class="page-link" href="#">2</a></li>
+			<li class="page-item"><a class="page-link" href="#">3</a></li>
+			<li class="page-item">
+			  <a class="page-link" href="#" aria-label="Next">
+				<span aria-hidden="true">&raquo;</span>
+				<span class="sr-only">Suivant</span>
+			  </a>
+			</li>
+		  </ul>
+		</nav>
+	
+	<footer>
+		&copy; 2018 Solenn LARZUL - Ma Boutique	
+	</footer>
+	
+	
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
